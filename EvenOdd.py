ba2bf2c88febd9bff4e316de46582d76d3e8561a
md-nashir-kazi <kazinasir1071@gmail.com>
2025-10-26 14:04:57 +0600

    All Code are updated

---
+++ EvenOdd.py
@@ -0,0 +1,11 @@
+#Function for Even odd
+
+def evenOr_Odd():
+    a=int(input("Enter an integer number:"))
+    if(a%2==0):
+        print(a ," is a Even Number")
+    else:
+        print(a, " is an ODD number!")
+        
+        
+evenOr_Odd()
---
+++ Function.py
@@ -0,0 +1,35 @@
+# def sum_calculator():
+#    a = int(input("please enter the value of a:"))
+#    b =int(input("please enter the value of b:"))
+#    print(a+b)
+#    return a+b
+# print("We have a Funtion called Sum_Calculator")
+# l=sum_calculator()
+# print("End of Sum_calculator function")
+# print(l)
+
+
+#Now let's Creat a substrac function
+# def Sub_cal (a,b):
+#     #print(a-b)
+#     return a-b
+
+# print(Sub_cal(50,10))
+
+cities=["Dhaka","rajbari","khulna","Faridpur","Bagura"]
+Nums=[5,10,20,30,40,50,44,"Nashir",'N']
+
+# def print_len(list):
+#     print(len(list))
+    
+
+# print_len(cities)
+# print_len(Nums)
+
+def print_list(list):
+    for item in list:
+        print(item, end=" ")
+        
+print_list(Nums)
+
+
